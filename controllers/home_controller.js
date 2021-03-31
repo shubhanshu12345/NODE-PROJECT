db6becb506fd55e7b9bc85e5a1c9cf256b8cff0f
shubhanshu12345 <shubhanshusharma.co@gmail.com>
2021-03-31 19:12:31 +0530

    SECOND COMMIT: ADDING INDEX.JS IN  ROUTES AND HOME CONTROLLER.JS IN CONTROLLERS

---
+++ controllers/home_controller.js
@@ -0,0 +1,5 @@
+module.exports.home=function(req,res){
+    return res.end("<h1> HOME </h1>")
+}
+
+// module.exports.ACTION_NAME=function(req,res){}
---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const express=require("express");
+const router=express.Router();
+const homeController=require("../controllers/home_controller");
+
+
+console.log("ROUTER IS LOADED");
+
+router.get('/home',homeController.home);
+
+module.exports=router;
